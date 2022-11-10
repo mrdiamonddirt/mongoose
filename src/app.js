5b7cc31db1d94231ec3fe0000a89aea6934ce40d
RowDog <mrdiamonddirt@gmail.com>
2022-11-10 12:16:31 +0000

    👩‍💻 Added all functions

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 require('./db/connection');
 const yargs = require('yargs');
 const mongoose = require('mongoose');
-const {createMovie} = require('./movies/functions');
+const {createMovie, readMovie, updateMovie, deleteMovie} = require('./movies/functions');
 
 async function myApp(yargsObject) {
     if (yargsObject.create) {
         //code to add a movie
         await createMovie({title: yargsObject.title, actor: yargsObject.actor, director: yargsObject.director});
     } else if (yargsObject.read) {
+        await readMovie();
         // code to list all movies
     } else if (yargsObject.update) {
         // code to update a movie
+        await updateMovie({title: yargsObject.title, actor: yargsObject.actor, director: yargsObject.director});
+        console.log(updateMovie)
     } else if (yargsObject.delete) {
         // code to delete a movie
+        await deleteMovie({title: yargsObject.title});
     } else {
         console.log('Please enter a valid command');
     };
--- src/movies/functions.js
+++ src/movies/functions.js
@@ -1,3 +1,4 @@
+const yargs = require('yargs');
 const MovieCollection = require('./model');
 
 async function createMovie(movieObject) {
@@ -9,4 +10,38 @@ async function createMovie(movieObject) {
     }
 }
 
-module.exports = {createMovie};
+async function readMovie() {
+    try {
+        const allMovies = await MovieCollection.find();
+        console.log(allMovies);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function updateMovie(movieObject) {
+    try {
+        const updatedMovie = await MovieCollection.findOneAndUpdate(yargs.argv.title, {
+            $set: {
+                title: yargs.argv.title,
+                actor: yargs.argv.actor,
+                director: yargs.argv.director,
+                new: true
+            }
+        });
+        console.log(updatedMovie);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteMovie(movieObject) {
+    try {
+        const deletedMovie = await MovieCollection.findOneAndDelete(movieObject);
+        console.log(deletedMovie);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {createMovie, readMovie, updateMovie, deleteMovie};

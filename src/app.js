6d6169cb546b30f50b5379f05fa07303e64ec709
RowDog <mrdiamonddirt@gmail.com>
2022-11-10 11:43:17 +0000

    👨‍💻function for create add to app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 require('./db/connection');
 const yargs = require('yargs');
 const mongoose = require('mongoose');
-
+const {createMovie} = require('./movies/functions');
 
 async function myApp(yargsObject) {
-    
     if (yargsObject.create) {
         //code to add a movie
+        await createMovie({title: yargsObject.title, actor: yargsObject.actor, director: yargsObject.director});
     } else if (yargsObject.read) {
         // code to list all movies
     } else if (yargsObject.update) {
